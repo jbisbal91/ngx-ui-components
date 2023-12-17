ed1c5cfb874e7eeb5e0eef92d57b46d54f7356f7
jbisbal91 <jbisbal91@gmail.com>
2023-12-17 11:40:01 -0700

    refactoring

--- src/app/modules/component/component.module.ts
+++ src/app/modules/component/component.module.ts
@@ -14,6 +14,7 @@ import { ComponentCategoryListComponent } from './components/component-category-
 import { ComponentNavComponent } from './components/component-nav/component-nav.component';
 import { ComponentSidenavComponent } from './pages/component-sidenav/component-sidenav.component';
 import { ComponentViewerComponent } from './components/component-viewer/component-viewer.component';
+import { GuidesComponent } from '../guides/pages/guides/guides.component';
 import { TabDocsComponent } from './docs/tab-docs/tab-docs.component';
 import {
   ButtonModule,
@@ -26,16 +27,15 @@ import {
 import { TabDemo1Component } from './demos/tabs-demos/tab-demo1/tab-demo1.component';
 import { DialogDocsComponent } from './docs/dialog-docs/dialog-docs.component';
 import { DialogDemo1Component } from './demos/dialog-demos/dialog-demo1/dialog-demo1.component';
-import { ButtonDemo1Component } from './demos/button/button-demo1/button-demo1.component';
 import { ButtonDocsComponent } from './docs/button-docs/button-docs.component';
+import { ButtonDemo1Component } from './demos/button/button-demo1/button-demo1.component';
 import { ButtonDemo2Component } from './demos/button/button-demo2/button-demo2.component';
 import { ButtonDemo3Component } from './demos/button/button-demo3/button-demo3.component';
 import { ButtonDemo4Component } from './demos/button/button-demo4/button-demo4.component';
-import { AccordionDemo1Component } from './demos/accordion/accordion-demo1/accordion-demo1.component';
 import { AccordionDocsComponent } from './docs/accordion-docs/accordion-docs.component';
+import { AccordionDemo1Component } from './demos/accordion/accordion-demo1/accordion-demo1.component';
 import { DividerDocsComponent } from './docs/divider-docs/divider-docs.component';
 import { DividerDemo1Component } from './demos/divider/divider-demo1/divider-demo1.component';
-import { GuidesComponent } from '../guides/pages/guides/guides.component';
 
 export function getHighlightLanguages() {
   return {
