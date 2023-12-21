95997f1f53c59fb8ce4882153eb5f9dc4e18f839
jbisbal91 <jbisbal91@gmail.com>
2023-12-21 05:00:03 -0700

    minor fix

--- projects/ngx-ui-components/src/lib/components/tab/tab.module.ts
+++ projects/ngx-ui-components/src/lib/components/tab/tab.module.ts
@@ -3,19 +3,9 @@ import { CommonModule } from '@angular/common';
 import { TabComponent } from './tab/tab.component';
 import { TabGroupComponent } from './tab-group/tab-group.component';
 
-
-
 @NgModule({
-  declarations: [
-    TabComponent,
-    TabGroupComponent
-  ],
-  exports: [
-    TabGroupComponent,
-    TabComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [TabComponent, TabGroupComponent],
+  exports: [TabGroupComponent, TabComponent],
+  imports: [CommonModule],
 })
-export class TabModule { }
+export class TabModule {}
