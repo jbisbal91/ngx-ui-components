14eefaff46d3f4a9c9fc9627653a7b6a5cbf70eb
jbisbal91 <jbisbal91@gmail.com>
2023-12-21 04:26:20 -0700

    minor fix

--- projects/ngx-ui-components/src/lib/components/carousel/carousel.component.ts
+++ projects/ngx-ui-components/src/lib/components/carousel/carousel.component.ts
@@ -1,6 +1,5 @@
-import { Component, ContentChildren, OnInit, QueryList } from '@angular/core';
+import { Component, ContentChildren, QueryList } from '@angular/core';
 import { CarouselItemComponent } from './carousel-item/carousel-item.component';
-import { Carousel } from './carousel.interface';
 
 @Component({
   selector: 'ngx-carousel',
@@ -11,43 +10,18 @@ export class CarouselComponent {
   @ContentChildren(CarouselItemComponent)
   public carouselItems!: QueryList<CarouselItemComponent>;
 
-  animationToLeft: boolean = false;
-  animationToRigth: boolean = false;
-  animationRightToCenter: boolean = false;
-
   ngAfterContentInit(): void {
     this.carouselItems.first.isActive = true;
   }
 
   selectItem(carouselItem: CarouselItemComponent) {
-    if (carouselItem.disabled) {
-      return;
-    }
-    if (carouselItem === this.carouselItems.first) {
-      return this.animationmoveToRight(carouselItem);
-    }
-    return this.animationmoveToLeft(carouselItem);
-  }
-
-  animationmoveToLeft(carousel: CarouselItemComponent) {
-    this.animationToLeft = true;
-    setTimeout(() => {
-      this.animationToLeft = false;
-      carousel.isActive = true;
-      this.carouselItems.forEach((ci) => {
-        if (carousel.id !== ci.id) {
-          ci.isActive = false;
-        }
-      });
-    }, 300);
-  }
-
-  animationmoveToRight(carousel: CarouselItemComponent) {
-    this.animationToRigth = true;
-    this.carouselItems.forEach((ci) => (ci.isActive = false));
-    carousel.isActive = true;
-    setTimeout(() => {
-      this.animationToRigth = false;
-    }, 300);
+    carouselItem.isActive = true;
+    this.carouselItems.forEach((ci) => {
+      if (ci.id !== carouselItem.id) {
+        ci.isActive = false;
+      }
+    });
+    const element = document.getElementById(carouselItem.id);
+    element?.scrollIntoView({ behavior: 'smooth' });
   }
 }
