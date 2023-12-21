9a0046d86991b8592542d55579e160403258bb91
jbisbal91 <jbisbal91@gmail.com>
2023-12-21 16:36:27 -0700

    AutoPlay

--- projects/ngx-ui-components/src/lib/components/carousel/carousel.component.ts
+++ projects/ngx-ui-components/src/lib/components/carousel/carousel.component.ts
@@ -1,25 +1,51 @@
-import { Component, ContentChildren, QueryList } from '@angular/core';
+import {
+  AfterContentInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ContentChildren,
+  Input,
+  QueryList,
+  booleanAttribute,
+  numberAttribute,
+} from '@angular/core';
 import { CarouselItemComponent } from './carousel-item/carousel-item.component';
 
 @Component({
   selector: 'ngx-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CarouselComponent {
+export class CarouselComponent implements AfterContentInit {
   @ContentChildren(CarouselItemComponent)
   public carouselItems!: QueryList<CarouselItemComponent>;
+  @Input({ transform: booleanAttribute }) ngxAutoPlay: boolean = false;
+  @Input({ transform: numberAttribute }) ngxAutoPlaySpeed: number = 3000;
+
+  currentItem!: CarouselItemComponent;
+
+  constructor() {}
 
   ngAfterContentInit(): void {
     this.carouselItems.first.isActive = true;
+    this.currentItem = this.carouselItems.first;
+    if (this.ngxAutoPlay) {
+      this.autoPlay();
+    }
   }
 
-  selectItem(carouselItem: CarouselItemComponent) {
-    carouselItem.isActive = true;
-    this.carouselItems.forEach((ci) => {
-      if (ci.id !== carouselItem.id) {
-        ci.isActive = false;
-      }
+  autoPlay(index: number = 0) {
+    setTimeout(() => {
+      this.onClick(this.carouselItems.get(index));
+      index = index === this.carouselItems.length - 1 ? 0 : ++index;
+      this.autoPlay(index);
+    }, this.ngxAutoPlaySpeed);
+  }
+
+  onClick(carouselItem: any) {
+    this.carouselItems?.forEach((ci: any) => {
+      ci.isActive = ci.id === carouselItem.id ? true : false;
     });
     const element = document.getElementById(carouselItem.id);
     element?.scrollIntoView({ behavior: 'smooth' });
