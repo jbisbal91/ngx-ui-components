11d5a2e0148aaf695488b0131e4112d4f67d73c9
jbisbal91 <jbisbal91@gmail.com>
2024-01-04 15:15:18 -0700

    minor fix

--- projects/ngx-ui-components/pie-chart/pie-chart/pie-chart.component.ts
+++ projects/ngx-ui-components/pie-chart/pie-chart/pie-chart.component.ts
@@ -65,34 +65,4 @@ export class PieChartComponent implements OnInit {
       console.error('Contexto 2D nulo.');
     }
   }
-
-  drawPieChart2(valores: number[], colores: string[]) {
-    const ctx = this.context;
-
-    if (ctx) {
-      const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
-      const centroX = canvasEl.width / 2;
-      const centroY = canvasEl.height / 2;
-      const radio = Math.min(canvasEl.width, canvasEl.height) / 2 - 10;
-      const total = valores.reduce((acc, val) => acc + val, 0);
-
-      let inicioAngulo = -Math.PI / 2;
-
-      for (let i = 0; i < valores.length; i++) {
-        const porcentaje = valores[i] / total;
-        const angulo = Math.PI * 2 * porcentaje;
-
-        ctx.beginPath();
-        ctx.moveTo(centroX, centroY);
-        ctx.arc(centroX, centroY, radio, inicioAngulo, inicioAngulo + angulo);
-        ctx.closePath();
-        ctx.fillStyle = colores[i];
-        ctx.fill();
-
-        inicioAngulo += angulo;
-      }
-    } else {
-      console.error('Contexto 2D nulo.');
-    }
-  }
 }
