081a96d61fc6a6c55559218ba3e73c169c4d1d9f
jbisbal91 <jbisbal91@gmail.com>
2024-02-05 10:15:18 -0700

    minor fix

--- projects/ngx-eagle/drawer/drawer.component.ts
+++ projects/ngx-eagle/drawer/drawer.component.ts
@@ -3,6 +3,7 @@ import {
   Component,
   ElementRef,
   EventEmitter,
+  HostListener,
   Input,
   OnChanges,
   OnInit,
@@ -14,7 +15,12 @@ import { NgxDrawerPlacement } from './typings';
 
 @Component({
   selector: 'ngx-drawer',
-  template: ` <div *ngIf="ngxVisible" class="ngx-drawer-backdrop">
+  template: ` <div
+    #backdrop
+    (click)="closeDrawer($event)"
+    *ngIf="ngxVisible"
+    class="ngx-drawer-backdrop"
+  >
     <div
       #drawer
       class="ngx-drawer"
@@ -31,10 +37,11 @@ import { NgxDrawerPlacement } from './typings';
 })
 export class DrawerComponent implements OnInit, OnChanges {
   @Input() ngxVisible: boolean = false;
-  @Input() ngxPlacement: NgxDrawerPlacement = 'bottom';
+  @Input() ngxPlacement: NgxDrawerPlacement = 'left';
 
   @Output() readonly ngxOnClose = new EventEmitter<void>();
 
+  @ViewChild('backdrop') backdropRef!: ElementRef;
   @ViewChild('drawer') drawerRef!: ElementRef;
 
   ngOnInit(): void {}
@@ -47,15 +54,42 @@ export class DrawerComponent implements OnInit, OnChanges {
 
   openDrawer() {
     setTimeout(() => {
-      if (this.drawerRef) {
-        if (this.ngxPlacement === 'top' || this.ngxPlacement === 'bottom') {
-          this.drawerRef.nativeElement.style.transform = 'translateY(0px)';
-        }
-
-        if (this.ngxPlacement === 'right' || this.ngxPlacement === 'left') {
-          this.drawerRef.nativeElement.style.transform = 'translateX(0px)';
-        }
+      if (
+        this.drawerRef &&
+        (this.ngxPlacement === 'top' ||
+          this.ngxPlacement === 'bottom' ||
+          this.ngxPlacement === 'right' ||
+          this.ngxPlacement === 'left')
+      ) {
+        const axis =
+          this.ngxPlacement === 'top' || this.ngxPlacement === 'bottom'
+            ? 'Y'
+            : 'X';
+        this.drawerRef.nativeElement.style.transform = `translate${axis}(0px)`;
       }
     });
+  }
+
+  closeDrawer(event: Event) {
+    const clickedElement = event.target as HTMLElement;
+    const isClickOnParent = clickedElement === this.backdropRef.nativeElement;
+    const isClickOnChild =
+      this.drawerRef.nativeElement.contains(clickedElement);
+
+    if (isClickOnParent && !isClickOnChild) {
+      const transformMap = {
+        bottom: 'translateY(100%)',
+        top: 'translateY(-100%)',
+        right: 'translateX(100%)',
+        left: 'translateX(-100%)',
+      };
+
+      this.drawerRef.nativeElement.style.transform =
+        transformMap[this.ngxPlacement];
+
+      setTimeout(() => {
+        this.ngxOnClose.emit();
+      }, 500);
+    }
   }
 }
