57b49aa379ebaefb34db20ac7f3a0630851d947c
jbisbal91 <jbisbal91@gmail.com>
2024-02-06 16:57:28 -0700

    minor fix

--- projects/ngx-eagle/rate/rate.component.ts
+++ projects/ngx-eagle/rate/rate.component.ts
@@ -18,18 +18,50 @@ export class RateComponent implements OnInit {
   @ViewChild('rateCanvas', { static: true })
   canvas!: ElementRef<HTMLCanvasElement>;
   context: CanvasRenderingContext2D | null = null;
+  selectedStar: number = -1;
   stars: boolean[] = [false, false, false, false, false]; // Estado de las estrellas (true = seleccionada, false = no seleccionada)
 
   ngOnInit(): void {
     this.context = this.canvas.nativeElement.getContext('2d');
     this.drawStars();
+    this.canvas.nativeElement.addEventListener('mousemove', (event) => {
+      this.onMouseMove(event);
+    });
+  }
+
+  onMouseMove(event: MouseEvent) {
+    const rect = this.canvas.nativeElement.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    let index = this.selectedStar; // Inicializar index como -1 por defecto
+
+    // Verificar si la posición y está dentro del rango de las estrellas
+    if (y >= 3 && y <= 33 && x < rect.width && y < rect.height) {
+      // Verificar la posición x en cada estrella
+      if (x >= 3 && x <= 35) {
+        index = 0;
+      } else if (x >= 44 && x <= 75) {
+        index = 1;
+      } else if (x >= 84 && x <= 115) {
+        index = 2;
+      } else if (x >= 124 && x <= 155) {
+        index = 3;
+      } else if (x >= 164 && x <= 195) {
+        index = 4;
+      }
+    }
+    const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
+    this.context?.clearRect(0, 0, canvasEl.width, canvasEl.height);
+    this.stars = this.stars.map((star, i) => i <= index);
+    this.drawStars(); // Volver a dibujar las estrellas con el nuevo estado
   }
 
   onCanvasClick(event: MouseEvent) {
     const rect = this.canvas.nativeElement.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
-    const index = Math.floor(x / 40); // Calcular el índice de la estrella según la posición x
+    const index = Math.floor(x / 40);    
+    this.selectedStar = index;
     this.onStarClick(index);
   }
 
@@ -86,7 +118,7 @@ export class RateComponent implements OnInit {
         this.context.fill();
       }
 
-      this.context.lineWidth = 2;
+      this.context.lineWidth = 1;
       this.context.strokeStyle = '#FADB14'; // Color del borde de la estrella
       this.context.stroke();
     }
