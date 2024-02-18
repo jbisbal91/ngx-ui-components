a6c3581fed3b63da85ef8c900de9bc031c6b6da3
jbisbal91 <jbisbal91@gmail.com>
2024-02-18 16:43:15 -0700

    minor fix

--- src/app/modules/component/demos/timeline/timeline-demo3/timeline-demo3.component.ts
+++ src/app/modules/component/demos/timeline/timeline-demo3/timeline-demo3.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-timeline-demo3',
   templateUrl: './timeline-demo3.component.html',
-  styleUrls: ['./timeline-demo3.component.scss']
+  styleUrls: ['./timeline-demo3.component.scss'],
 })
-export class TimelineDemo3Component {
-
-}
+export class TimelineDemo3Component {}
