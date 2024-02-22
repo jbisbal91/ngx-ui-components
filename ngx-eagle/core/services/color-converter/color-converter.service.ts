d5a094edfea982d6948a0408752b63f721b19078
jbisbal91 <jbisbal91@gmail.com>
2024-02-22 11:17:36 -0700

    minor fix

--- ngx-eagle/core/services/color-converter/color-converter.service.ts
+++ ngx-eagle/core/services/color-converter/color-converter.service.ts
@@ -44,10 +44,20 @@ export class ColorConverter {
   //--------------END VALIDATIONS ------------------
 
   contrastingColors(color: string): ColorContrast {
+    let rgb: RGB;
+
+    if(this.isHex(color)){
+      rgb = this.hexToRgb(color);
+    }
+
+    if(this.isHSL(color)){
+      rgb = this.hslToRgb(color);
+    }
+    
     const getContrastColor = (color: string) => {
-      const luminance = this.luminance(this.hexToRgb(color));
+      const luminance = this.luminance(rgb);
       return luminance > 0.5
-        ? this.changeRgbLuminance(this.hexToRgb(color), 0.35)
+        ? this.changeRgbLuminance(rgb, 0.35)
         : '#ffffff';
     };
     const backgroundColor = color;
@@ -86,32 +96,36 @@ export class ColorConverter {
     if (!hsl) throw new TypeError(`Invalid argument; has no value.`);
     const match = hsl.match(/^hsl\(\s*(\d+)\s*,\s*(\d+)%\s*,\s*(\d+)%\s*\)$/);
     if (!match) throw new TypeError(`Invalid HSL color format: ${hsl}`);
-    
+
     const h = parseInt(match[1]) / 360;
     const s = parseInt(match[2]) / 100;
     const l = parseInt(match[3]) / 100;
 
     let r, g, b;
     if (s === 0) {
-        r = g = b = l; // Achromatic
+      r = g = b = l; // Achromatic
     } else {
-        const hue2rgb = (p: number, q: number, t: number) => {
-            if (t < 0) t += 1;
-            if (t > 1) t -= 1;
-            if (t < 1 / 6) return p + (q - p) * 6 * t;
-            if (t < 1 / 2) return q;
-            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-            return p;
-        };
-        const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-        const p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1 / 3);
-        g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1 / 3);
+      const hue2rgb = (p: number, q: number, t: number) => {
+        if (t < 0) t += 1;
+        if (t > 1) t -= 1;
+        if (t < 1 / 6) return p + (q - p) * 6 * t;
+        if (t < 1 / 2) return q;
+        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+        return p;
+      };
+      const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      const p = 2 * l - q;
+      r = hue2rgb(p, q, h + 1 / 3);
+      g = hue2rgb(p, q, h);
+      b = hue2rgb(p, q, h - 1 / 3);
     }
 
-    return { r: Math.round(r * 255), g: Math.round(g * 255), b: Math.round(b * 255) };
-}
+    return {
+      r: Math.round(r * 255),
+      g: Math.round(g * 255),
+      b: Math.round(b * 255),
+    };
+  }
 
   //------------- END -------------
   //------------- TO HSL -------------
