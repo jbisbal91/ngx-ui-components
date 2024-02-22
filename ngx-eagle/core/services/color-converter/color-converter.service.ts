ae82bef64b85c93322c948760e70c16bfa939c4c
jbisbal91 <jbisbal91@gmail.com>
2024-02-22 13:10:05 -0700

    minor fix

--- ngx-eagle/core/services/color-converter/color-converter.service.ts
+++ ngx-eagle/core/services/color-converter/color-converter.service.ts
@@ -38,7 +38,7 @@ export class ColorConverter {
   public isHSLA(hsla: string): boolean {
     if (!hsla) throw new TypeError(`Invalid argument; has no value.`);
     const regex =
-      /^hsla\(\s*((\d{1,2}|[1-2]\d{2}|3[0-5]\d)|360)\s*,\s*(\d{1,2}|100)%\s*,\s*(\d{1,2}|100)%\s*,\s*(0?\.\d+|1(\.0)?)\s*\)$/;
+      /^hsla\(\s*((\d{1,2}|[1-2]\d{2}|3[0-5]\d)|360)\s*,\s*(\d{1,2}|100)%\s*,\s*(\d{1,2}|100)%\s*,\s*(0(\.\d+)?|1(\.0)?)\s*\)$/;
     return regex.test(hsla);
   }
   //--------------END VALIDATIONS ------------------
@@ -48,15 +48,24 @@ export class ColorConverter {
     if (this.isHex(color)) {
       rgb = this.hexToRgb(color);
     }
+
+    if (this.isRGB(color)) {
+      rgb = this.rgbToObject(color);
+    }
+
     if (this.isHSL(color)) {
       rgb = this.hslToRgb(color);
     }
-    const buildOverlayColor = (color: string) => {
+    if (this.isHSLA(color)) {
+      rgb = this.hslaToRgb(color);
+    }
+
+    const buildOverlayColor = () => {
       const luminance = this.luminance(rgb);
       return luminance > 0.5 ? this.changeRgbLuminance(rgb, 0.35) : '#ffffff';
     };
     const backgroundColor = color;
-    const overlayColor = buildOverlayColor(color);
+    const overlayColor = buildOverlayColor();
     return { backgroundColor: backgroundColor, overlayColor: overlayColor };
   }
 
@@ -72,6 +81,20 @@ export class ColorConverter {
   }
 
   //------------- TO RGB -------------
+  rgbToObject(rgb: string): RGB {
+    if (!rgb) throw new TypeError(`Invalid argument; has no value.`);
+    const match = rgb.match(
+      /^rgb\(\s*(0*(?:[0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*,\s*(0*(?:[0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*,\s*(0*(?:[0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5]))\s*\)$/
+    );
+    if (!match) throw new TypeError(`Invalid RGB color format: ${rgb}`);
+
+    return {
+      r: parseInt(match[1]),
+      g: parseInt(match[2]),
+      b: parseInt(match[3]),
+    };
+  }
+
   hexToRgb(hex: string): RGB {
     let hexCode = hex.startsWith('#') ? hex.slice(1) : hex;
     if (hexCode.length === 3) {
@@ -115,6 +138,40 @@ export class ColorConverter {
       b = hue2rgb(p, q, h - 1 / 3);
     }
 
+    return {
+      r: Math.round(r * 255),
+      g: Math.round(g * 255),
+      b: Math.round(b * 255),
+    };
+  }
+
+  hslaToRgb(hsla: string): RGB {
+    if (!hsla) throw new TypeError(`Invalid argument; has no value.`);
+    const match = hsla.match(
+      /^hsla\(\s*((\d{1,2}|[1-2]\d{2}|3[0-5]\d)|360)\s*,\s*(\d{1,2}|100)%\s*,\s*(\d{1,2}|100)%\s*,\s*(0(\.\d+)?|1(\.0)?)\s*\)$/
+    );
+    if (!match) throw new TypeError(`Invalid HSLA color format: ${hsla}`);
+    const h = parseInt(match[1]) / 360;
+    const s = parseInt(match[2]) / 100;
+    const l = parseInt(match[3]) / 100;
+    let r, g, b;
+    if (s === 0) {
+      r = g = b = l; // Achromatic
+    } else {
+      const hue2rgb = (p: number, q: number, t: number) => {
+        if (t < 0) t += 1;
+        if (t > 1) t -= 1;
+        if (t < 1 / 6) return p + (q - p) * 6 * t;
+        if (t < 1 / 2) return q;
+        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+        return p;
+      };
+      const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      const p = 2 * l - q;
+      r = hue2rgb(p, q, h + 1 / 3);
+      g = hue2rgb(p, q, h);
+      b = hue2rgb(p, q, h - 1 / 3);
+    }
     return {
       r: Math.round(r * 255),
       g: Math.round(g * 255),
