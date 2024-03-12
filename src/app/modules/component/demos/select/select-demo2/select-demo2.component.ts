6936d39a8ebaa8fe9a77937c68fe909f895c1d54
jbisbal91 <jbisbal91@gmail.com>
2024-03-11 22:36:22 -0700

    minor fix

--- src/app/modules/component/demos/select/select-demo2/select-demo2.component.ts
+++ src/app/modules/component/demos/select/select-demo2/select-demo2.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./select-demo2.component.scss']
 })
 export class SelectDemo2Component {
+  value = 'opt1';
+  options: { value: string; label: string }[] = [];
+  constructor() {
+    for (let i = 1; i <= 100; i++) {
+      this.options.push({ value: `opt${i}`, label: `Option ${i}` });
+    }
+  }
 
+  onChangeValue(value: any) {
+    this.value = value;
+  }
 }
