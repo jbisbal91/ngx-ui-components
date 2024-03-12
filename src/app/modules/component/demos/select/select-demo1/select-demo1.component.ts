27ebd958e88cd85bcbc54dd9ba8b5c9662ee7b0b
jbisbal91 <jbisbal91@gmail.com>
2024-03-11 17:40:30 -0700

    minor fix

--- src/app/modules/component/demos/select/select-demo1/select-demo1.component.ts
+++ src/app/modules/component/demos/select/select-demo1/select-demo1.component.ts
@@ -9,7 +9,7 @@ export class SelectDemo1Component {
   value = 'opt1';
   options: { value: string; label: string }[] = [];
   constructor() {
-    for (let i = 1; i <= 1000; i++) {
+    for (let i = 1; i <= 100; i++) {
       this.options.push({ value: `opt${i}`, label: `Option ${i}` });
     }
   }
