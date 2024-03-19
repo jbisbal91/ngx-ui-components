79426fb0be402818c1bd664219e749c7f579dd34
jbisbal91 <jbisbal91@gmail.com>
2024-03-19 08:07:58 -0700

    minor fix

--- ngx-eagle/table/public-api.ts
+++ ngx-eagle/table/public-api.ts
@@ -2,3 +2,4 @@ export * from './table.module';
 export * from './table/table.directive';
 export * from './table/column-group.directive';
 export * from './table/sort.directive';
+export * from './table/resize.directive';
