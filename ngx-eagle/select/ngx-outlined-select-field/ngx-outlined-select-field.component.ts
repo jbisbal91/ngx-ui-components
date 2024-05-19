f0cfdc6cc965a0aa4ed6ae55c1384c0011c0b30f
jbisbal91 <jbisbal91@gmail.com>
2024-05-19 13:01:27 -0700

    minor fix

--- ngx-eagle/select/ngx-outlined-select-field/ngx-outlined-select-field.component.ts
+++ ngx-eagle/select/ngx-outlined-select-field/ngx-outlined-select-field.component.ts
@@ -18,8 +18,6 @@ import {
   booleanAttribute,
 } from '@angular/core';
 import { ControlValueAccessor, NgControl } from '@angular/forms';
-import { Guid, StylesService } from 'ngx-eagle/core/services';
-import { ErrorColor } from 'ngx-eagle/core/types';
 import { Subscription } from 'rxjs';
 import { NgxOptionComponent } from '../ngx-option/ngx-option.component';
 import { SelectedOption } from './selected-option.interface';
@@ -32,18 +30,15 @@ import { SelectedOption } from './selected-option.interface';
   imports: [NgIf, NgTemplateOutlet],
 })
 export class NgxOutlinedSelectFieldComponent
-  implements AfterViewInit, ControlValueAccessor, OnDestroy
-{
+  implements AfterViewInit, ControlValueAccessor, OnDestroy {
   @ContentChildren(NgxOptionComponent)
   public optionList!: QueryList<NgxOptionComponent>;
 
   @Input({ transform: booleanAttribute }) autocomplete: boolean = false;
   @Input({ transform: booleanAttribute }) disabled: boolean = false;
-  @Input() label!: string;
   @Input({ transform: booleanAttribute }) multiple: boolean = false;
   @Input() pattern!: any;
   @Input() placeholder: string = '';
-  @Input() prefix!: any | TemplateRef<void>;
   @Input({ transform: booleanAttribute }) required!: boolean;
   @Input() suffix!: any | TemplateRef<void>;
 
@@ -68,48 +63,41 @@ export class NgxOutlinedSelectFieldComponent
   }
 
   isSelectedOption(
-    elemento: any
-  ): elemento is SelectedOption | SelectedOption[] {
-    if (Array.isArray(elemento)) {
-      return elemento.every((item) => this.isSelectedOption(item));
+    element: any
+  ): element is SelectedOption | SelectedOption[] {
+    if (Array.isArray(element)) {
+      return element.every((item) => this.isSelectedOption(item));
     } else {
       return (
-        typeof elemento === 'object' &&
-        'label' in elemento &&
-        'value' in elemento
+        typeof element === 'object' &&
+        'label' in element &&
+        'value' in element
       );
     }
   }
 
   @Output() onChangeValue: EventEmitter<any> = new EventEmitter<any>();
 
-  _placeholder: string = '';
-  errorText: string = '';
 
-  public inputPrefixId: string = Guid.create();
+
 
   optPos: 'bottom' | 'top' = 'bottom';
 
-  @ViewChild('input_container') containerRef!: ElementRef;
-  @ViewChild('input_label') labelRef!: ElementRef;
+
   @ViewChild('input') inputRef!: ElementRef;
   @ViewChild('options_container') optionsRef!: ElementRef;
 
-  borderColor: string = '#747775';
-  onChange: any = () => {};
-  onTouched: any = () => {};
+  onChange: any = () => { };
+  onTouched: any = () => { };
 
   private subscription: Subscription = new Subscription();
 
-  isValid: boolean = true;
   isFocused: boolean = false;
-  autofilled: boolean = false;
   isOpenMultipleMode: boolean = false;
 
   constructor(
     public elementRef: ElementRef,
     private renderer: Renderer2,
-    private stylesService: StylesService,
     @Optional() @Self() public ngControl: NgControl
   ) {
     if (this.ngControl) {
@@ -122,8 +110,6 @@ export class NgxOutlinedSelectFieldComponent
   }
 
   ngAfterViewInit() {
-    this.customProperties();
-    this.initialize();
     this.optionList.forEach((option) => {
       option.checked = this.multiple;
       this.subscription.add(
@@ -146,7 +132,6 @@ export class NgxOutlinedSelectFieldComponent
   mousedown(event: any): void {
     this.isOpenMultipleMode =
       this.multiple && this.optionsRef.nativeElement.contains(event.target);
-    this.moveLabel();
   }
 
   onKeyDown(event: KeyboardEvent) {
@@ -155,43 +140,8 @@ export class NgxOutlinedSelectFieldComponent
     }
   }
 
-  customProperties() {
-    const styles: any = {
-      'border-color': (value: any) => {
-        this.borderColor = value;
-        this.renderer.setStyle(
-          this.containerRef.nativeElement,
-          'border-color',
-          value
-        );
-      },
-    };
-
-    for (const style in styles) {
-      if (styles.hasOwnProperty(style)) {
-        const value = this.stylesService.getStyleValue(
-          this.elementRef.nativeElement,
-          style
-        );
-        styles[style](value);
-      }
-    }
-  }
-
   typeOf(value: any) {
     return typeof value;
-  }
-
-  initialize() {
-    if (this.multiple) {
-      this.autocomplete = false;
-    }
-    this.moveLabel();
-    setTimeout(() => {
-      this.errorText =
-        this.elementRef?.nativeElement.attributes['error-text']?.value;
-      this.ngControl?.control?.setValue(this.internalValue);
-    });
   }
 
   writeValue(value: any): void {
@@ -214,10 +164,6 @@ export class NgxOutlinedSelectFieldComponent
     this.onTouched = fn;
   }
 
-  setDisabledState(isDisabled: boolean): void {
-    this.disabled = isDisabled;
-  }
-
   singleInputValue(value: any) {
     if (typeof value !== 'string' && typeof value !== 'number') {
       throw new TypeError(
@@ -230,7 +176,6 @@ export class NgxOutlinedSelectFieldComponent
         this.internalValue = opt.label;
         opt.selected = true;
         found = true;
-        this.moveLabel();
       } else {
         opt.selected = false;
       }
@@ -258,7 +203,6 @@ export class NgxOutlinedSelectFieldComponent
         });
       });
       this.selectMultipleOptions(this.selectedOptions);
-      this.moveLabel();
     }
   }
 
@@ -293,60 +237,11 @@ export class NgxOutlinedSelectFieldComponent
       }
     });
     this.selectMultipleOptions(this.selectedOptions);
-    this.moveLabel();
-  }
-
-  moveLabel() {
-    const containerHeight = this.containerRef?.nativeElement.offsetHeight;
-    if (
-      (this.isFocused ||
-        this.isValidValue(this.internalValue) ||
-        this.isValidValue(this.inputRef.nativeElement.value)) &&
-      this.label
-    ) {
-      this.applyFocusedStyle();
-    } else {
-      this.applyDefaultStyle(containerHeight);
-    }
-  }
-
-  private applyFocusedStyle() {
-    this.setLabelStyle('-0.375rem', '0.75rem');
-    this._placeholder = this.placeholder;
-    this.buildBorderOutlined();
-  }
-
-  private applyDefaultStyle(containerHeight: number) {
-    const height = (containerHeight - 14) / 2;
-    this.setLabelStyle(`${height / 16}rem`, '0.875rem');
-    this._placeholder = '';
-    this.drawLineTopBorder();
-  }
-
-  private setLabelStyle(top: string, fontSize: string) {
-    const left =
-      this.prefix && !this.isFocused && !this.internalValue && !this.autofilled
-        ? `${this.prefixWidth()}px`
-        : '0.75rem';
-    this.renderer.setStyle(this.labelRef?.nativeElement, 'top', top);
-    this.renderer.setStyle(this.labelRef?.nativeElement, 'font-size', fontSize);
-    this.renderer.setStyle(this.labelRef?.nativeElement, 'left', left);
-  }
-
-  prefixWidth() {
-    const prefix = document.getElementById(this.inputPrefixId);
-    const result = prefix ? prefix?.offsetWidth : 0;
-    return result;
   }
 
   onInput(event: Event): void {
     if (this.autocomplete) {
       this.onSearch(event);
-    }
-    if (this.label) {
-      this.buildBorderOutlined();
-    } else {
-      this.drawLineTopBorder();
     }
   }
 
@@ -366,7 +261,6 @@ export class NgxOutlinedSelectFieldComponent
 
   onFocus(event: FocusEvent) {
     this.isFocused = true;
-    this.moveLabel();
     this.showBackdrop();
     this.adjustOptionsPosition();
   }
@@ -377,7 +271,7 @@ export class NgxOutlinedSelectFieldComponent
   }
 
   adjustOptionsPosition() {
-    const contProp = this.containerRef.nativeElement.getBoundingClientRect();
+    const contProp = this.inputRef.nativeElement.getBoundingClientRect();
     const optionsProp = this.optionsRef.nativeElement.getBoundingClientRect();
     let top = contProp.top + contProp.height;
     const offsetTop = contProp.top;
@@ -417,71 +311,20 @@ export class NgxOutlinedSelectFieldComponent
   onBlur(event: FocusEvent) {
     setTimeout(() => {
       this.isFocused = false;
-      this.validate();
-      this.moveLabel();
       this.hideBackdrop();
     }, 100);
   }
 
-  buildBorderOutlined() {
-    const percent = this.calculateBorderPercent();
-    const background = this.calculateBackgroundStyle(percent);
-    this.setBorderTop('unset');
-    this.renderer.setStyle(
-      this.containerRef.nativeElement,
-      'background',
-      background
-    );
-  }
-
-  private calculateBorderPercent(): number {
-    const containerWidth = this.containerRef.nativeElement.offsetWidth;
-    const labelWidth = this.labelRef.nativeElement.offsetWidth;
-    return ((labelWidth + 16) / containerWidth) * 100;
-  }
-
-  private calculateBackgroundStyle(percent: number): string {
-    const color = this.isValid ? this.borderColor : ErrorColor;
-    return `linear-gradient(to right, ${color} 8px, transparent 8px, transparent ${percent}%, ${color} ${percent}%) no-repeat top/100% 1px`;
-  }
-
-  drawLineTopBorder() {
-    const color = this.isValid ? this.borderColor : ErrorColor;
-    this.setBorderTop('unset');
-    this.renderer.setStyle(
-      this.containerRef.nativeElement,
-      'background',
-      `linear-gradient(to right, transparent 0%, ${color} 0%) no-repeat top/100% 1px`
-    );
-  }
-
-  setBorderTop(border: string) {
-    this.renderer.setStyle(
-      this.containerRef.nativeElement,
-      'border-top',
-      border
-    );
-  }
-
-  validate() {
-    this.isValid =
-      (!this.pattern && !this.ngControl && !this.required) ||
-      this.ngControl?.status?.toLowerCase() === 'valid' ||
-      (this.required &&
-        this.isValidValue(this.internalValue) &&
-        !this.ngControl) ||
-      (this.pattern && this.inputRef.nativeElement.validity.valid)
-        ? true
-        : false;
-    const color = this.isValid ? this.borderColor : ErrorColor;
-    this.renderer.setStyle(
-      this.containerRef.nativeElement,
-      'border-color',
-      color
-    );
-  }
-
   isValidValue(value: any): boolean {
     return value !== undefined && value !== null && value !== '';
+  }
+
+  valid() {
+    return (!this.pattern && !this.ngControl && !this.required) ||
+      this.ngControl?.status?.toLowerCase() === 'valid' ||
+      (this.required && this.isValidValue(this.value) && !this.ngControl) ||
+      (this.pattern && this.inputRef.nativeElement.validity.valid)
+      ? true
+      : false;
   }
 }
--- src/app/modules/component/demos/select/select-demo1/select-demo1.component.ts
+++ src/app/modules/component/demos/select/select-demo1/select-demo1.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-select-demo1',
@@ -6,17 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./select-demo1.component.scss'],
 })
 export class SelectDemo1Component {
-  value:any= [];
+  form!: FormGroup;
+  value: any = [];
   options: { value: string; label: string }[] = [];
-  constructor() {
+
+  constructor(private formBuilder: FormBuilder) {
     for (let i = 1; i <= 10; i++) {
       this.options.push({ value: `opt${i}`, label: `Option ${i}` });
       this.value.push({ value: `opt${i}`, label: `Option ${i}` });
     }
   }
 
+  ngOnInit(): void {
+    this.initFormGroup();
+  }
+
   onChangeValue(value: any) {
     this.value = value;
     console.log(value);
+  }
+
+  initFormGroup() {
+    this.form = this.formBuilder.group({
+      select: [
+        this.value,
+        [
+          Validators.required,
+        ],
+      ]
+    });
   }
 }
